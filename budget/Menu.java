6f09f1942ae3edec29e68b8d6872add0a267a27f
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-09-21 00:07:22 -0400

    Updated the Menu class so that it acccomodates PurchaseType objects as one of its fields

--- budget/Menu.java
+++ budget/Menu.java
@@ -4,10 +4,15 @@ import java.util.*;
 
 class Menu {
     private float income;
-    private Map<String, Float> purchases;
+    private Map<String, PurchaseType> purchases;
+    static Scanner scanner = new Scanner(System.in);
 
     Menu() {
-        this.purchases = new LinkedHashMap<>();
+        this.purchases = new HashMap<>();
+        this.purchases.put("Food", new Food());
+        this.purchases.put("Clothes", new Clothes());
+        this.purchases.put("Entertainment", new Entertainment());
+        this.purchases.put("Other", new Other());
         this.income = 0;
     }
 
@@ -22,7 +27,7 @@ class Menu {
         else return 0;
     }
 
-    public Map<String, Float> getPurchases() {
+    public Map<String, PurchaseType> getPurchases() {
         return this.purchases;
     }
 
@@ -30,29 +35,84 @@ class Menu {
         this.income = income;
     }
 
-    protected void addPurchase(String item, float price) {
-        this.purchases.put(item, price);
-    }
-
-    protected void showPurchases() {
-        if (this.purchases.isEmpty()) {
-            System.out.println("The purchase list is empty\n");
-        } else {
-            for (var entry: this.purchases.entrySet()) {
-                System.out.println(entry.getKey() + " $" + entry.getValue());
-            }
-
-            System.out.println("Total sum: $" + this.calculateTotalSum() + "\n");
-        }
-    }
-
     private float calculateTotalSum() {
         float sum = 0;
 
-        for (float price: this.purchases.values()) {
-            sum += price;
+        for (PurchaseType purchase: this.purchases.values()) {
+            sum += purchase.calculateTotalSum();
         }
 
         return sum;
     }
+
+    public void showPurchases() {
+        if (this.isEmpty()) {
+            System.out.println("The purchase list is empty!");
+        } else {
+            boolean status = true;
+            while (status) {
+                showPurchaseMenu();
+                int userInput = scanner.nextInt();
+                System.out.println();
+
+                PurchaseType purchase;
+
+                switch (userInput) {
+                    case 1 -> {
+                        purchase = this.purchases.get("Food");
+                        purchase.showPurchases();
+                    }
+                    case 2 -> {
+                        purchase = this.purchases.get("Clothes");
+                        purchase.showPurchases();
+                    }
+                    case 3 -> {
+                        purchase = this.purchases.get("Entertainment");
+                        purchase.showPurchases();
+                    }
+                    case 4 -> {
+                        purchase = this.purchases.get("Other");
+                        purchase.showPurchases();
+                    }
+                    case 5 -> this.printAllPurchases();
+                    case 6 -> {
+                        status = false;
+                        System.out.println();
+                    }
+                }
+            }
+        }
+    }
+
+    private void printAllPurchases() {
+        System.out.println("All:");
+
+        for (PurchaseType purchase: this.purchases.values()) {
+            for (var entry: purchase.purchases.entrySet()) {
+                System.out.println(entry.getKey() + " $" + entry.getValue());
+            }
+        }
+
+        System.out.println("Total sum: $" + this.calculateTotalSum() + "\n");
+    }
+
+    private static void showPurchaseMenu() {
+        System.out.println("""
+            Choose the type of purchase
+            1) Food
+            2) Clothes
+            3) Entertainment
+            4) Other
+            5) All
+            6) Back""");
+    }
+
+    protected boolean isEmpty() {
+        boolean verdict = true;
+
+        for (PurchaseType purchase: this.purchases.values()) if (!purchase.isEmpty()) verdict = false;
+
+        return verdict;
+    }
+
 }
