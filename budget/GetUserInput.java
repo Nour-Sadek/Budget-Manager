775f979fece3047a2ff3e7abf766df0616102483
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-09-30 11:39:12 -0400

    Added more functions that check for correct user input

--- budget/GetUserInput.java
+++ budget/GetUserInput.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class GetUserInput {
 
-    static final String[] PRINT_MENU_OPTIONS = {"0", "1", "2", "3", "4", "5", "6"};
+    static final String[] PRINT_MENU_OPTIONS = {"0", "1", "2", "3", "4", "5", "6", "7"};
     static final String[] PRINT_PURCHASE_OPTIONS = {"1", "2", "3", "4", "5"};
     static final String[] PRINT_PURCHASE_MENU_OPTIONS = {"1", "2", "3", "4", "5", "6"};
+    static final String[] PRINT_ANALYZE_OPTIONS = {"1", "2", "3", "4"};
+    static final String[] PRINT_CERTAIN_TYPES_OPTIONS = {"1", "2", "3", "4"};
 
     static int getMenuInput() {
         while (true) {
@@ -40,7 +42,7 @@ public class GetUserInput {
             String userInput = Main.scanner.nextLine();
 
             if (!Arrays.asList(PRINT_PURCHASE_MENU_OPTIONS).contains(userInput)) {
-                System.out.println("Wrong integer input!");
+                System.out.println("Wrong integer input!\n");
             } else {
                 return Integer.parseInt(userInput);
             }
@@ -65,6 +67,32 @@ public class GetUserInput {
             return number >= 0;
         } catch (NumberFormatException e) {
             return false;
+        }
+    }
+
+    static int getAnalyzeOptionsInput() {
+        while (true) {
+            Main.printAnalyzeMenu();
+            String userInput = Main.scanner.nextLine();
+
+            if (!Arrays.asList(PRINT_ANALYZE_OPTIONS).contains(userInput)) {
+                System.out.println("Wrong integer input!\n");
+            } else {
+                return Integer.parseInt(userInput);
+            }
+        }
+    }
+
+    static int getCertainTypeInput() {
+        while (true) {
+            Main.printCertainTypeMenu();
+            String userInput = Main.scanner.nextLine();
+
+            if (!Arrays.asList(PRINT_CERTAIN_TYPES_OPTIONS).contains(userInput)) {
+                System.out.println("Wrong integer input!\n");
+            } else {
+                return Integer.parseInt(userInput);
+            }
         }
     }
 }
