e5d7a4d9b176f19ccf6dc57e01b573e057119a5d
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-09-30 11:36:44 -0400

    Added the functionality to analyze the purchases

--- budget/Main.java
+++ budget/Main.java
@@ -24,6 +24,7 @@ public class Main {
                 case 4 -> showBalance();
                 case 5 -> savePurchases();
                 case 6 -> loadPurchases();
+                case 7 -> analyze();
             }
         }
     }
@@ -37,6 +38,7 @@ public class Main {
                 4) Balance
                 5) Save
                 6) Load
+                7) Analyze (Sort)
                 0) Exit""");
     }
 
@@ -159,5 +161,162 @@ public class Main {
         } catch (FileNotFoundException e) {
             System.out.println("No file found: purchases.txt");
         }
+    }
+
+    private static void analyze() {
+        while (true) {
+            int userInput = GetUserInput.getAnalyzeOptionsInput();
+            System.out.println();
+
+            if (userInput == 4) break;
+
+            switch (userInput) {
+                case 1:
+                    sortAllPurchases();
+                    break;
+                case 2:
+                    sortByType();
+                    break;
+                case 3:
+                    sortCertainType();
+                    break;
+            }
+        }
+    }
+
+    private static void sortCertainType() {
+        int userInput = GetUserInput.getCertainTypeInput();
+        System.out.println();
+
+        PurchaseType purchase = switch (userInput) {
+            case 1 -> menu.getPurchases().get("Food");
+            case 2 -> menu.getPurchases().get("Clothes");
+            case 3 -> menu.getPurchases().get("Entertainment");
+            case 4 -> menu.getPurchases().get("Other");
+            default -> null;
+        };
+
+        if (purchase.isEmpty()) {
+            System.out.println("The purchase list is empty!\n");
+        } else {
+            String className = purchase.getClass().getSimpleName();
+            Map<String, Float> items = new TreeMap<>();
+            Map<String, Float> purchases = purchase.getPurchases();
+
+            for (var entry: purchases.entrySet()) {
+                items.put(entry.getKey(), entry.getValue());
+            }
+
+            Map<String, Float> sortedAllItems = sortByValues(items);
+
+            System.out.println(className + ':');
+            printAllPurchases(sortedAllItems);
+
+        }
+    }
+
+    private static void sortByType() {
+        System.out.println("Types:");
+
+        Map<String, PurchaseType> purchases = menu.getPurchases();
+        Map<String, Float> purchaseTypes = new TreeMap<>();
+        float total = 0;
+
+        for (var purchaseType: purchases.entrySet()) {
+            PurchaseType purchase = purchaseType.getValue();
+            String className = purchase.getClass().getSimpleName();
+            float purchaseTotal = purchase.calculateTotalSum();
+            total += purchaseTotal;
+            purchaseTypes.put(className, purchaseTotal);
+        }
+
+        for (var entry: sortByValues(purchaseTypes).entrySet()) {
+            System.out.println(entry.getKey() + " - $" + String.format("%.2f", entry.getValue()));
+        }
+
+        System.out.println("Total sum: $" + String.format("%.2f", total) + "\n");
+    }
+
+    private static void sortAllPurchases() {
+        if (menu.isEmpty()) {
+            System.out.println("The purchase list is empty!\n");
+        } else {
+            Map<String, Float> allItems = new TreeMap<>();
+            Map<String, PurchaseType> purchases = menu.getPurchases();
+
+            for (var purchaseType: purchases.entrySet()) {
+                PurchaseType purchase = purchaseType.getValue();
+                // Check if this purchase type has items
+                if (!purchase.isEmpty()) {
+                    Map<String, Float> items = purchase.getPurchases();
+                    for (var entry: items.entrySet()) {
+                        allItems.put(entry.getKey(), entry.getValue());
+                    }
+                }
+            }
+
+            Map<String, Float> sortedAllItems = sortByValues(allItems);
+
+            System.out.println("All:");
+            printAllPurchases(sortedAllItems);
+        }
+    }
+
+    private static void printAllPurchases(Map<String, Float> purchases) {
+
+        for (var entry: purchases.entrySet()) {
+            System.out.println(entry.getKey() + " $" + String.format("%.2f", entry.getValue()));
+        }
+
+        System.out.println("Total sum: $" + String.format("%.2f", calculateTotalSum(purchases)) + "\n");
+    }
+
+    private static float calculateTotalSum(Map<String, Float> purchases) {
+        float sum = 0;
+
+        for (float purchase: purchases.values()) {
+            sum += purchase;
+        }
+
+        return sum;
+    }
+
+    protected static void printAnalyzeMenu() {
+        System.out.println("""
+                How do you want to sort?
+                1) Sort all purchases
+                2) Sort by type
+                3) Sort certain type
+                4) Back""");
+    }
+
+    // Sort TreeMap by values rather than by keys
+    public static <K, V extends Comparable<V>> Map<K, V>
+    sortByValues(final Map<K, V> map) {
+        Comparator<K> valueComparator =
+                new Comparator<K>() {
+                    public int compare(K k1, K k2) {
+                        int compare =
+                                map.get(k1).compareTo(map.get(k2));
+                        if (compare == 0)
+                            return 1;
+                        else
+                            return -compare;
+                    }
+                };
+
+        Map<K, V> sortedByValues =
+                new TreeMap<K, V>(valueComparator);
+        sortedByValues.putAll(map);
+        return sortedByValues;
+    }
+
+    protected static void printCertainTypeMenu() {
+        System.out.println("""
+                Choose the type of purchase
+                1) Food
+                2) Clothes
+                3) Entertainment
+                4) Other""");
     }
 }
